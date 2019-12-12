3c35010feef2c3475cb98d5088a2d29a701c7ca2
Pancho Labrador <plabradorjr@gmail.com>
2019-12-12 06:27:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,7 +89,7 @@ class Student
     end
   end
 
-  def self.first_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT *
       FROM students
