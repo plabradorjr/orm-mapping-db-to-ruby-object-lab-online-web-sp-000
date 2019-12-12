e57dbb4cfb9ad48ab8010489e6adaabf821eb3cc
Pancho Labrador <plabradorjr@gmail.com>
2019-12-12 06:19:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,18 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+      LIMIT 1
+    SQL
 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 
 end
